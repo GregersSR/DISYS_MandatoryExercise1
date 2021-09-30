22737a72d36b664e085b10e6584c73175236f1d1
Gregers Rørdam <gregers@rordam.dk>
2021-09-30 14:44:41 +0200

    Implement in-memory storage capability

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/gregerssr/DISYS_MandatoryExercise1
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- rest/storage/storage.go
+++ rest/storage/storage.go
@@ -1,8 +1,44 @@
-package api
+package storage
 
-var 
+import (
+	"errors"
 
-func InsertCourse(c Course) {}
-func GetCourse(c Course)    {}
-func UpdateCourse(c Course) {}
-func DeleteCourse(c Course) {}
+	"github.com/gregerssr/DISYS_MandatoryExercise1/rest/model"
+)
+
+var courses map[int64]model.Course
+
+func Init() {
+	courses = make(map[int64]model.Course)
+}
+func InsertCourse(c model.Course) bool {
+	if _, inMap := courses[c.Id]; !inMap {
+		courses[c.Id] = c
+		return true
+	}
+	return false
+}
+
+func GetCourse(id int64) (model.Course, error) {
+	course, inMap := courses[id]
+	if inMap {
+		return course, nil
+	} else {
+		return course, errors.New("Course doesn't exist")
+	}
+}
+
+func UpdateCourse(c model.Course) bool {
+	if _, inMap := courses[c.Id]; inMap {
+		courses[c.Id] = c
+		return true
+	}
+	return false
+}
+func DeleteCourse(id int64) bool {
+	if _, inMap := courses[id]; inMap {
+		delete(courses, id)
+		return true
+	}
+	return false
+}
