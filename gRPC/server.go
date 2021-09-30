9e3525dc73c4022e195fe6de497c82bf3e85f512
Christoffer Gram <chkg@itu.dk>
2021-09-30 19:38:04 +0200

    Add First server.go Implementation
    Knwon issue: Cannot be executed with go run because server.go is not in the
    main package. However, given the folder structure it must
    be in the gRPC package.

--- gRPC/server.go
+++ gRPC/server.go
@@ -1,5 +1,38 @@
 package gRPC
 
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+// Must implement the CourseServer interface
 type Server struct {
-	Storage Repo
+	UnimplementedCourseServer
+
+	Repo Repo
+}
+
+const (
+	port = ":50051"
+)
+
+func (s *Server) FindCourse(ctx context.Context, in *CourseRequest) (*CourseReply, error) {
+	log.Printf("Recieved %v", in.GetId())
+	return &CourseReply{Name: s.Repo.FindCourse(in.GetId()).Title}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	RegisterCourseServer(s, &Server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.26.0
 )
