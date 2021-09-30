8e081b51f143cb0961fd30772d7dfd1b52cbf7c8
Christoffer Gram <chkg@itu.dk>
2021-09-30 20:22:11 +0200

    Move storage.go to api Pkg + Init with Course
    Init() method now adds a course with ID 1 to the Courses map.

--- gRPC/api/storage.go
+++ gRPC/api/storage.go
@@ -1,11 +1,11 @@
-package gRPCAPI
+package api
 
 type Course struct {
 	ID                int64
 	Title             string
 	Teachers          []int64
 	ECTS              int
-	SatisfactionStore int
+	SatisfactionScore int
 }
 
 type Repo struct {
@@ -14,6 +14,9 @@ type Repo struct {
 
 func (r *Repo) Init() {
 	r.Courses = make(map[int64]Course)
+
+	course := Course{ID: 1, Title: "DISYS", Teachers: []int64{1, 2}, ECTS: 10, SatisfactionScore: 10}
+	r.Courses[course.ID] = course
 }
 
 func (r *Repo) FindCourse(id int64) Course {
